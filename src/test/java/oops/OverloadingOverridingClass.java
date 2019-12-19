bcbaea7334c0f6abab10669e8c988818efcd811c
Sanjeev Saxena <sanrocks123@gmail.com>
2019-12-20 03:16:02 +0530

    some test refactorring

--- src/test/java/oops/OverloadingOverridingClass.java
+++ src/test/java/oops/OverloadingOverridingClass.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2019 @SanRockzz Ltd. All Rights Reserved.
  */
 
-package designpatterns;
+package oops;
 
 /**
  * Java Source OverloadingOverridingClass.java created on Dec 2, 2019
